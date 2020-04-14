70d4fbc2ae8742a7a5175067a8c4e72a5a9af5cd
Sugayathri <sugayathri100@gmail.com>
2020-04-14 16:54:12 +0530

    static variables access without object creation

---
+++ staticvar/src/localvar/StaticVariables.java
@@ -0,0 +1,12 @@
+package localvar;
+
+public class StaticVariables {
+	public static int a=10;
+	public static int b=20;
+	public static void main(String[] args) {
+		
+		System.out.println(a+b);
+
+	}
+
+}
