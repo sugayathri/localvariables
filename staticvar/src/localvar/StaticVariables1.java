274f318bdba4b9e8fc051b43a97341fb0c2e17fd
Sugayathri <sugayathri100@gmail.com>
2020-04-14 17:07:53 +0530

    Static variables in other class and other package

---
+++ staticvar/src/localvar/StaticVariables1.java
@@ -0,0 +1,11 @@
+package localvar;
+
+public class StaticVariables1 {
+
+	public static void main(String[] args) {
+		//classname.variablename
+		System.out.println(StaticVariables.a*StaticVariables.b);
+
+	}
+
+}
---
+++ staticvar/src/localvar1/StaticVariables2.java
@@ -0,0 +1,13 @@
+package localvar1;
+
+import localvar.StaticVariables;
+
+public class StaticVariables2 {
+
+	public static void main(String[] args) {
+		//classname.variablename
+		System.out.println(StaticVariables.a+StaticVariables.b);
+
+	}
+
+}
